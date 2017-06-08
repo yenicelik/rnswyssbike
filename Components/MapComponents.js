e343b1c826a9f49f7d182de9ff674925d888efe9
yenicelik <david.yenicelik@gmail.com>
2017-06-08 20:50:33 +0200

    Implemented GPS. Should move this into a separate file if possible

--- Components/MapComponents.js
+++ Components/MapComponents.js
@@ -9,27 +9,93 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Dimensions
 } from 'react-native';
 
 import MapView from 'react-native-maps';
 
+const {
+  width,
+  height
+} = Dimensions.get('window');
+const SCREEN_HEIGHT = height;
+const SCREEN_WIDTH = width;
+const ASPECT_RATIO = width / height;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+
 export default class rnswyssbike extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      gpsPosition: {
+        latitude: 0,
+        longitude: 0
+      }
+    }
+  }
+
+  /* START OF GPS LOGIC */
+  watchID: ? number = null;
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition((position) => {
+      var lat = parseFloat(position.coords.latitude);
+      var lng = parseFloat(position.coords.longitude);
+      var initialRegion = {
+        latitude: lat,
+        longitude: lng
+      };
+      this.setState({
+        gpsPosition: initialRegion
+      });
+
+    }, (error) => alert(JSON.stringify(error)), {
+      enableHighAccuracy: true,
+      timeout: 2000,
+      maximumAge: 1000
+    });
+
+    this.watchID = navigator.geolocation.watchPosition((position) => {
+      var lat = parseFloat(position.coords.latitude);
+      var lng = parseFloat(position.coords.longitude);
+      var lastRegion = {
+        latitude: lat,
+        longitude: lng
+      };
+      this.setState({
+        gpsPosition: lastRegion
+      }, (error) => console.log(JSON.stringify(error)), {
+        enableHighAccuracy: true,
+        timeout: 2000,
+        maximumAge: 1000
+      });
+
+    });
+  }
+
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchID);
+  }
+  /* END OF GPS LOGIC */ //TODO: Figure out how to move this into a separate component
+
   render() {
     return (
       <View style={styles.container}>
         <MapView
         style={styles.map}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
+        region={{
+          latitude: this.state.gpsPosition.latitude,
+          longitude: this.state.gpsPosition.longitude,
+          latitudeDelta: LATITUDE_DELTA,
+          longitudeDelta: LONGITUDE_DELTA,
         }}>
         <MapView.Marker
         coordinate={{
-          latitude: 37.78825,
-          longitude: -122.4324
+          latitude: this.state.gpsPosition.latitude,
+          longitude: this.state.gpsPosition.longitude
         }}>
         <View style={styles.radius}>
         <View style={styles.marker} />
@@ -43,13 +109,13 @@ export default class rnswyssbike extends Component {
 
 const styles = StyleSheet.create({
   radius: {
-    height: 50,
-    width: 50,
-    borderRadius: 50/2,
+    height: 30,
+    width: 30,
+    borderRadius: 30/2,
     overflow: 'hidden',
-    backgroundColor: 'rgba(0, 122, 255, 0.1)',
-    borderWidth: 1,
-    borderColor: 'rgba(0, 112, 255, 0.3)',
+    backgroundColor: 'rgba(0, 122, 255, 0.5)',
+    borderWidth: 4,
+    borderColor: 'rgba(0, 122, 255, 0.5)',
     alignItems: 'center',
     justifyContent: 'center'
   },
@@ -60,7 +126,7 @@ const styles = StyleSheet.create({
     borderColor: 'white',
     borderRadius: 20/2,
     overflow: 'hidden',
-    backgroundColor: '#F5FCFF'
+    backgroundColor: '#rgba(255, 255, 255, 0.05)'
   },
   container: {
     flex: 1,
--- index.ios.js
+++ index.ios.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import {AppRegistry} from 'react-native';
 
-import MapComponent from './Components/MapComponents'
-AppRegistry.registerComponent('Sample', () => MapComponent);
+import MapComponents from './Components/MapComponents'
+AppRegistry.registerComponent('Sample', () => MapComponents); //TODO changing this to rnswyssbike doesn't have any effects
