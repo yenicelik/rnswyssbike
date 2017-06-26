bc603e841d93c23815055c4164b7b1e7b54d35b7
Michael <spacephoton@gmail.com>
2017-06-26 20:30:16 +0300

    added all essential files, works on android

---
+++ app/index.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import {
+    AppRegistry
+} from 'react-native';
+
+import { Navigation } from 'react-native-navigation';
+import { registerScreens } from './screens.js';
+
+registerScreens();
+
+Navigation.startSingleScreenApp({
+    screen: {
+        screen: 'rnswyssbike.LoginComponent', // unique ID registered with Navigation.registerScreen
+        navigatorStyle: {}, // override the navigator style for the screen, see "Styling the navigator" below (optional)
+        navigatorButtons: {} // override the nav buttons for the screen, see "Adding buttons to the navigator" below (optional)
+    }
+})
+
